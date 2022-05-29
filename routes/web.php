3dc4301c38750a15b49a1737371a13a21095c68d
juan camilo <juancamilofrancocardona@gmail.com>
2022-05-29 12:50:59 -0500

    img, post

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,10 @@ Route::post('/register', [RegisterController::class, 'store']);
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'store']);
-
 Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
 
 Route::get('/{user:username}', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 
 Route::post('/imagenes', [ImagenController::class, 'store'])->name('imagenes.store');
