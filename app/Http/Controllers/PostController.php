d048325b86f0e423de8e007650b93cc4559e27df
juan camilo <juancamilofrancocardona@gmail.com>
2022-06-02 21:02:35 -0500

    comentarios

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,7 +10,7 @@ class PostController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth')->except('show', 'index');
     }
     
     public function index( User $user ) 
@@ -66,7 +66,8 @@ class PostController extends Controller
     public function show(User $user, Post $post)
     {
         return view('posts.show', [
-            'post' => $post
+            'post' => $post, 
+            'user' => $user
         ]);
     }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('contenido')
-    <div class="container mx-auto flex">
+    <div class="container mx-auto md:flex">
         <div class="md:w-1/2">
             <img src=" {{ asset('uploads') . '/' . $post->imagen }} " alt="Imagen del post {{ $post->titulo }}">
 
@@ -22,30 +22,44 @@
             
         <div class="md:w-1/2 p-5">
             <div class="shadow bg-white p-5 mb-5">
-                <p class="text-xl font-bold text-center mb-4">Agrega un nuevo Comentario</p>
 
-                <form action="">
-                    <div class="mb-5">
-                        <label for="comentario" class="mb-2 block uppercase text-gray-500 font-bold">
-                            Comentario
-                        </label>
-                        <textarea
-                            id="comentario"
-                            name="comentario"
-                            placeholder="Descripción Publicaión"
-                            class="border p-3 w-full rounded-lg @error('comentario') border-red-500 @enderror"
-                        ></textarea>
-                        @error('comentario')
-                            <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center"> {{ $message }} </p>
-                        @enderror
-                    </div>
+                @auth
 
-                    <input
-                        type="submit"
-                        value="Comentar"
-                        class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg"
-                    />
-                </form>
+                    <p class="text-xl font-bold text-center mb-4">Agrega un nuevo Comentario</p>
+
+                    @if(session('mensaje'))
+                        <div class="bg-green-500 p-2 rounded-lg mb-6 text-white text-center uppercase font-bold" >
+                            {{ session( 'mensaje' ) }}
+                        </div>
+                    @endif
+
+                    <form action=" {{ route( 'comentarios.store', [ 'post' => $post, 'user' => $user ] ) }} " method="POST">
+                        @csrf
+
+                        <div class="mb-5">
+                            <label for="comentario" class="mb-2 block uppercase text-gray-500 font-bold">
+                                Comentario
+                            </label>
+                            <textarea
+                                id="comentario"
+                                name="comentario"
+                                placeholder="Descripción Publicaión"
+                                class="border p-3 w-full rounded-lg @error('comentario') border-red-500 @enderror"
+                            ></textarea>
+                            @error('comentario')
+                                <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center"> {{ $message }} </p>
+                            @enderror
+                        </div>
+
+                        <input
+                            type="submit"
+                            value="Comentar"
+                            class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg"
+                        />
+                    </form>
+
+                @endauth
+
             </div>
         </div>
     </div>
