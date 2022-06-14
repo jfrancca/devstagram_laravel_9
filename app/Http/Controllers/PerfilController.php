2c20f48b03796a240c1a04822d9d1db9f85ebd16
juan camilo <juancamilofrancocardona@gmail.com>
2022-06-13 19:32:15 -0500

    perfil

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class PerfilController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        // return view('perfil.index');
+        dd('desde perfil');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\ImagenController;
 use App\Http\Controllers\LikeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\PerfilController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
@@ -34,3 +35,7 @@ Route::post('/imagenes', [ImagenController::class, 'store'])->name('imagenes.sto
 // Like a las fotos
 Route::post('/posts/{post}/likes', [LikeController::class, 'store'])->name('posts.likes.store');
 Route::delete('/posts/{post}/likes', [LikeController::class, 'destroy'])->name('posts.likes.destroy');
+
+// Rutas para el perfil
+Route::get('{user:username}/editar-perfil', [PerfilController::class, 'index'])->name('perfil.index');
+Route::post('{user:username}/editar-perfil', [PerfilController::class, 'store'])->name('perfil.store');
